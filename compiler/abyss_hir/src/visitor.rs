19e4d4fd1cbf41e28819e2086f3570156d66dfa5
jhosein58 <jhosein58@gmail.com>
2026-07-25 03:31:15 +0330

    feat: add abyss_typer crate

--- compiler/abyss_hir/src/visitor.rs
+++ compiler/abyss_hir/src/visitor.rs
@@ -1,6 +1,6 @@
 use abyss_nexus::nexus::Nexus;
 
-use crate::hir::HirProgram;
+use crate::hir::{HirExprKind, HirProgram};
 
 pub struct HirCursor<'a> {
     nexus: &'a Nexus,
@@ -15,5 +15,17 @@ impl<'a> HirCursor<'a> {
             program,
             pointer_stack: vec![program.root as usize],
         }
+    }
+
+    pub fn current_type(&self) -> HirExprKind {
+        self.program.kinds[self.pointer_stack.last().copied().unwrap()]
+    }
+
+    pub fn left(&self) -> HirExprKind {
+        self.program.kinds[self.program.lhs[self.pointer_stack.last().copied().unwrap()] as usize]
+    }
+
+    pub fn right(&self) -> HirExprKind {
+        self.program.kinds[self.program.rhs[self.pointer_stack.last().copied().unwrap()] as usize]
     }
 }
