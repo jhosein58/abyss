230c01371f1864550cb5c1c635ec41ba767ffa07
jhosein58 <jhosein58@gmail.com>
2026-08-10 11:03:21 +0330

    feat(nexus): add alloc_* functions to type storage

--- compiler/abyss_nexus/src/storages/types.rs
+++ compiler/abyss_nexus/src/storages/types.rs
@@ -1,23 +1,26 @@
 use std::collections::HashMap;
 
-use abyss_types::{TypeKind, TypeStore};
+use abyss_types::{TyKind, TyStore};
 
 use crate::nexus::TypeId;
 
+#[repr(u8)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub enum TypeKey {
     Unknown,
 
-    Int(u16), // integer type with bit width
+    Int(u16), // bit width
     UInt(u16),
     Float(u16),
-
     Bool,
+
+    Ptr(TypeId),
 }
 
 #[derive(Default)]
 pub struct TypeStorage {
-    store: TypeStore,
-    interned: HashMap<TypeKey, TypeId>,
+    store: TyStore,
+    interned: HashMap<TypeKey, TypeId>, // FIXME: hash-map inja mitone sari tar beshe ya kolan hazf beshe
 }
 
 impl TypeStorage {
@@ -32,7 +35,45 @@ impl TypeStorage {
     }
 
     #[inline(always)]
-    pub fn kind(&self, idx: TypeId) -> TypeKind {
+    pub fn kind(&self, idx: TypeId) -> TyKind {
         self.store.kinds[idx.0 as usize]
+    }
+
+    #[inline(always)]
+    fn get_or_insert(&mut self, key: TypeKey, kind: TyKind, payload: u32) -> TypeId {
+        *self
+            .interned
+            .entry(key)
+            .or_insert_with(|| TypeId(self.store.push(kind, payload) as u32))
+    }
+
+    #[inline(always)]
+    pub fn alloc_unknown(&mut self) -> TypeId {
+        self.get_or_insert(TypeKey::Unknown, TyKind::Unknown, 0)
+    }
+
+    #[inline(always)]
+    pub fn alloc_int(&mut self, width: u16) -> TypeId {
+        self.get_or_insert(TypeKey::Int(width), TyKind::Int, width as u32)
+    }
+
+    #[inline(always)]
+    pub fn alloc_uint(&mut self, width: u16) -> TypeId {
+        self.get_or_insert(TypeKey::UInt(width), TyKind::UInt, width as u32)
+    }
+
+    #[inline(always)]
+    pub fn alloc_float(&mut self, width: u16) -> TypeId {
+        self.get_or_insert(TypeKey::Float(width), TyKind::Float, width as u32)
+    }
+
+    #[inline(always)]
+    pub fn alloc_bool(&mut self) -> TypeId {
+        self.get_or_insert(TypeKey::Bool, TyKind::Bool, 0)
+    }
+
+    #[inline(always)]
+    pub fn alloc_ptr(&mut self, inner: TypeId) -> TypeId {
+        self.get_or_insert(TypeKey::Ptr(inner), TyKind::Ptr, inner.0)
     }
 }
--- compiler/abyss_types/src/lib.rs
+++ compiler/abyss_types/src/lib.rs
@@ -1,23 +1,25 @@
 #[repr(u8)]
 #[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum TypeKind {
+pub enum TyKind {
     #[default]
     Unknown,
 
     Int,
-    Uint,
+    UInt,
     Float,
     Bool,
+
+    Ptr,
 }
 
 #[derive(Default)]
-pub struct TypeStore {
-    pub kinds: Vec<TypeKind>,
+pub struct TyStore {
+    pub kinds: Vec<TyKind>,
     pub payload: Vec<u32>,
     pub extra: Vec<u32>,
 }
 
-impl TypeStore {
+impl TyStore {
     #[inline(always)]
     pub fn len(&self) -> usize {
         self.kinds.len()
@@ -28,5 +30,13 @@ impl TypeStore {
         self.kinds.reserve(additional);
         self.payload.reserve(additional);
         self.extra.reserve(additional);
+    }
+
+    #[inline(always)]
+    pub fn push(&mut self, tykind: TyKind, payload: u32) -> usize {
+        let idx = self.len();
+        self.kinds.push(tykind);
+        self.payload.push(payload);
+        idx
     }
 }
