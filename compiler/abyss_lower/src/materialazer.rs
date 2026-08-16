6c35f2614946bd2ee9c0a45fd0e35c33b54c0d47
jhosein58 <jhosein58@gmail.com>
2026-08-16 06:08:16 +0330

    refactor(tc): rename check_var function to check_binding_and_var

--- compiler/abyss_lower/src/materialazer.rs
+++ compiler/abyss_lower/src/materialazer.rs
@@ -1,14 +1,18 @@
+use abyss_hir::hir::HirExprKind as Hir;
 use abyss_nexus::nexus::{HirId, Nexus, SymbolId};
 
 pub fn build(db: &Nexus, symbol: SymbolId) {
     let range = db.symbol_hir_range.get_copy(symbol);
+    build_expr(db, range.end);
+}
 
-    let HirId(offset) = range.start;
-    let HirId(end) = range.end;
+fn build_expr(db: &Nexus, id: HirId) {
+    let kind = db.hir.kind(id);
 
-    for i in 0..=(end - offset) {
-        let _id = HirId(i + offset);
-
-        // println!("{:?}", db.hir.kind(id))
+    match kind {
+        Hir::Binding => {
+            let value_type = db.hir.extra(id)
+        }
+        _ => {}
     }
 }
