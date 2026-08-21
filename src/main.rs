e631a9c8a6fe2c61f2e9ada40849740bb3045397
jhosein58 <jhosein58@gmail.com>
2026-08-21 18:34:30 +0330

    refactor(lower): remove builder trait

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
 pub use std::{fs, time::Instant};
 
-use abyss_codegen::cranelift::codegen::CraneliftBackend;
 use abyss_engine::engine::Engine;
 use abyss_typer::tyck::TyCtx;
 
 fn main() {
-    let t = Instant::now();
+    // let t = Instant::now();
 
-    let mut eng: Engine<CraneliftBackend> = Engine::new();
+    // let mut eng: Engine<CraneliftBackend> = Engine::new();
 
-    let file_id = eng.add_file("main.a", fs::read_to_string("main.a").unwrap());
+    // let file_id = eng.add_file("main.a", fs::read_to_string("main.a").unwrap());
 
-    let sym_id = eng.get_symbol_id(file_id, "main");
+    // let sym_id = eng.get_symbol_id(file_id, "main");
 
-    eng.type_of(sym_id);
+    // eng.type_of(sym_id);
 
-    eng.db.dump_hir();
-    eng.print_err();
+    // eng.db.dump_hir();
+    // eng.print_err();
 
-    println!("{:?}", t.elapsed());
+    // println!("{:?}", t.elapsed());
 
     //     let res = eng.run(sym_id);
     //     println!("main says: {}", res);
