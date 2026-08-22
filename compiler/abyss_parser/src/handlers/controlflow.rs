d916f83ede82bac53b3dedfcea27636aab6e908e
jhosein58 <jhosein58@gmail.com>
2026-08-23 03:24:54 +0330

    feat(parser): write if parsing logic

--- compiler/abyss_parser/src/handlers/controlflow.rs
+++ compiler/abyss_parser/src/handlers/controlflow.rs
@@ -1,10 +1,20 @@
 use abyss_nexus::{arena::ArenaId, nexus::HirId};
+use abyss_token::kind::TokenKind as Tk;
 
 use crate::parser::Parser;
 
 impl Parser<'_> {
     pub fn parse_if(&mut self) -> HirId {
         self.bump();
+
+        let cond = self.parse_expr(0);
+        let thenb = self.parse_expr(0);
+
+        let mut elseb = None;
+
+        if self.optional(Tk::Else) {
+            elseb = Some(self.parse_expr(0))
+        }
 
         HirId::none()
     }
