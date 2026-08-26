7ecef06872e95dd2e98d2eefe54e6ade1d206902
jhosein58 <jhosein58@gmail.com>
2026-08-26 20:15:35 +0330

    feat(parser): parse struct init

--- compiler/abyss_parser/src/handlers/structs.rs
+++ compiler/abyss_parser/src/handlers/structs.rs
@@ -1,9 +1,9 @@
 use std::u32;
 
-use abyss_nexus::{arena::ArenaId, nexus::HirId};
+use abyss_nexus::nexus::HirId;
 use abyss_token::kind::TokenKind::{self as Tk, CBrace};
 
-use crate::parser::Parser;
+use crate::{parser::Parser, precedence::Precedence};
 
 impl Parser<'_> {
     #[inline]
@@ -48,8 +48,34 @@ impl Parser<'_> {
         self.bump(); // .
         self.expect(Tk::OBrace);
 
+        let mut fields = vec![];
+        let mut values = vec![];
+
+        loop {
+            if self.peek() == Some(Tk::CBrace) {
+                break;
+            }
+
+            let f = self.parse_expr(Precedence::VarDef.value() + 1);
+            self.expect(Tk::Colon);
+            let v = self.parse_expr(0);
+
+            fields.push(f.0);
+            values.push(v.0);
+
+            if self.peek() == Some(CBrace) {
+                break;
+            }
+            if !self.peek_preceded_by_newline() {
+                self.expect(Tk::Comma);
+            }
+        }
+
         self.expect(Tk::CBrace);
 
-        HirId::none()
+        let fids = self.db.add_list_flat(&fields);
+        let vids = self.db.add_list_flat(&values);
+
+        self.db.hir.alloc_struct_init(fids, vids)
     }
 }
